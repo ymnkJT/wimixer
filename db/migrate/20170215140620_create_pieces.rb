c384a206232bef17957628986f0fca242ab35269
ymnkJT <javadtomi@gmail.com>
2017-02-15 23:16:12 +0900

    Create pieces table

--- db/migrate/20170215140620_create_pieces.rb
+++ db/migrate/20170215140620_create_pieces.rb
@@ -1,7 +1,10 @@
 class CreatePieces < ActiveRecord::Migration
   def change
     create_table :pieces do |t|
-
+      t.string     :title
+      t.string     :composer
+      t.string     :arranger
+      t.string     :publisher
       t.timestamps null: false
     end
   end
