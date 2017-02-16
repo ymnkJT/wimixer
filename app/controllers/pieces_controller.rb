64741f1c82d5c51144b8d9799bbe6bacdd06730c
ymnkJT <javadtomi@gmail.com>
2017-02-16 21:57:41 +0900

    Set create and index action

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,5 +1,6 @@
 class PiecesController < ApplicationController
   def index
+    @pieces = Piece.all
   end
 
   def new
@@ -7,5 +8,16 @@ class PiecesController < ApplicationController
   end
 
   def create
+    @piece = Piece.new(pieces_params)
+    if @piece.save
+      redirect_to pieces_path, notice: "新たな曲を登録しました！"
+    else
+      render :new
+    end
   end
+
+  private
+    def pieces_params
+      params.require(:piece).permit(:title, :composer, :arranger, :publisher)
+    end
 end
