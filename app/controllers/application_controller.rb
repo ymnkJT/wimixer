84b209f9f196ed2d342662991d59194979af61a7
ymnkJT <javadtomi@gmail.com>
2017-02-18 23:21:41 +0900

    Permit nickname parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
@@ -7,5 +8,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     new_user_session_path
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,2 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  def new
-    super
-    render layout: 'another_layout'
-  end
 end
