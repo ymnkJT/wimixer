0188ac8de7b4ce1731ae2eec43a2784cfd40a4df
ymnkJT <javadtomi@gmail.com>
2017-02-18 19:30:59 +0900

    Create dropdown button at header and set icon

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,13 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
-
 Bundler.require(*Rails.groups)
 
 module Wimixer
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
     config.action_view.field_error_proc = proc { |html_tag, instance| html_tag }
   end
 end
+
+
