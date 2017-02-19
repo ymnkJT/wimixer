2cf962bc0af3a10e35090102f472d0d1c0169d46
ymnkJT <javadtomi@gmail.com>
2017-02-19 22:35:45 +0900

    Create comments table

--- db/migrate/20170219131331_create_comments.rb
+++ db/migrate/20170219131331_create_comments.rb
@@ -4,6 +4,7 @@ class CreateComments < ActiveRecord::Migration
       t.references :user, index: true, foreign_key: true
       t.references :piece, index: true, foreign_key: true
       t.text :content
+      t.boolean :play, default: false
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218051501) do
+ActiveRecord::Schema.define(version: 20170219131331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "piece_id"
+    t.text     "content"
+    t.boolean  "play",       default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "comments", ["piece_id"], name: "index_comments_on_piece_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "pieces", force: :cascade do |t|
     t.string   "title"
@@ -44,4 +56,6 @@ ActiveRecord::Schema.define(version: 20170218051501) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "pieces"
+  add_foreign_key "comments", "users"
 end
