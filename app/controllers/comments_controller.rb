965a054e745e13cc13c433bb87ee9a4a0edf25aa
ymnkJT <javadtomi@gmail.com>
2017-02-19 23:59:03 +0900

    Create new view of comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,21 @@
 class CommentsController < ApplicationController
-  def create
+  def new
+    @comment = Comment.new
+    @piece = @comment.piece
   end
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    @piece = @comment.piece
+    if @comment.save
+      redirect_to pieces_path, notice: "感想を記録しました！"
+    else
+      render :new
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:piece_id, :content, :play)
+    end
 end
