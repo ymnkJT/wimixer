0d9af4ebcaeb01600d8d3e9250f236365cb5d1b6
ymnkJT <javadtomi@gmail.com>
2017-03-05 15:28:58 +0900

    Fix images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,8 +3,8 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  # config.action_controller.perform_caching = true
+  config.serve_static_files = true
+  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
   config.assets.compile = true
   config.assets.digest = true
